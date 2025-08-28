585be734574f001bbf0d34a327ac85a2e0f6e97c
PhamVanTea <23633471.tra@student.iuh.edu.vn>
2025-08-28 23:21:30 +0700

    feat: change folder GUI and create folder for backend

---
+++ IUH-Phamarcity-Management-Backend/src/main/java/vn/edu/iuh/fit/IUH_Phamarcity_Management_Backend/IuhPhamarcityManagementBackendApplication.java
@@ -0,0 +1,13 @@
+package vn.edu.iuh.fit.IUH_Phamarcity_Management_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IuhPhamarcityManagementBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IuhPhamarcityManagementBackendApplication.class, args);
+	}
+
+}
---
+++ IUH-Phamarcity-Management-Backend/src/test/java/vn/edu/iuh/fit/IUH_Phamarcity_Management_Backend/IuhPhamarcityManagementBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.edu.iuh.fit.IUH_Phamarcity_Management_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IuhPhamarcityManagementBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
