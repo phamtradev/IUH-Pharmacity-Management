b2839a2d3f3fc5ce82cf9658a4886b8b6e43afd3
ATFIREE <anthuyen2k5@gmail.com>
2025-10-28 07:08:23 +0700

    fix(UI): Login fixed bugs

--- src/main/java/vn/edu/iuh/fit/iuhpharmacitymanagement/gui/application/loading/SplashScreen.java
+++ src/main/java/vn/edu/iuh/fit/iuhpharmacitymanagement/gui/application/loading/SplashScreen.java
@@ -30,6 +30,12 @@ public class SplashScreen extends javax.swing.JFrame {
      */
     public SplashScreen() {
         initComponents();
+        
+        BackgroundPanel.setComponentZOrder(progressBar, 0); // ProgressBar ở lớp trên cùng
+        BackgroundPanel.setComponentZOrder(lblLoading, 1);
+        //BackgroundPanel.setComponentZOrder(ProgressBar, 2);
+        BackgroundPanel.setComponentZOrder(IUHLogo, 3);
+        BackgroundPanel.setComponentZOrder(BackgroundImg, 3);
 
         setLocationRelativeTo(null);
         setResizable(false);
@@ -55,7 +61,7 @@ public class SplashScreen extends javax.swing.JFrame {
                 Thread.sleep(500);
                 
                 updateProgress("Chuẩn bị gd...", 30);
-                openLogin();
+                //openLogin();
                 Thread.sleep(500);
                 
                 updateProgress("Chuẩn bị gd...", 50);
