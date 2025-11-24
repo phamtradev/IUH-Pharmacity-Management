f99ddb39f408caef5529c8466487ac01deb5955a
PhamVanTea <23633471.tra@student.iuh.edu.vn>
2025-11-24 17:01:36 +0700

    comment: entity ChiTietDonNhapHang

--- src/main/java/vn/edu/iuh/fit/iuhpharmacitymanagement/entity/ChiTietDonNhapHang.java
+++ src/main/java/vn/edu/iuh/fit/iuhpharmacitymanagement/entity/ChiTietDonNhapHang.java
@@ -5,33 +5,34 @@ package vn.edu.iuh.fit.iuhpharmacitymanagement.entity;
  * @author PhamTra
  */
 public class ChiTietDonNhapHang {
+
     //private String maChiTietDonNhapHang;
     private int soLuong;
     private double donGia;
     private double thanhTien;
     private DonNhapHang donNhapHang;
     private LoHang loHang;
-    
+
     private double tyLeChietKhau;
-    private double tienChietKhau;
-    private double thanhTienTinhThue;
+    private double tienChietKhau; //dẫn xuất
+    private double thanhTienTinhThue; //dẫn xuất
     private double thueSuat;
-    private double tienThue;
+    private double tienThue; //dẫn xuất
     private double thanhTienSauThue;
-    
+
     public static final String MA_CHI_TIET_DON_NHAP_HANG_RONG = "Mã chi tiết đơn nhập hàng không được rỗng";
     public static final String SO_LUONG_SAI = "Số lượng không hợp lệ phải lớn hơn 0, không được rỗng";
     public static final String DON_GIA_SAI = "Đơn giá không hợp lệ phải lớn hơn hoặc bằng 0";
     public static final String THANH_TIEN_SAI = "Thành tiền không hợp lệ phải lớn hơn hoặc bằng 0";
     public static final String DON_NHAP_HANG_RONG = "Đơn nhập hàng không được rỗng";
     public static final String LO_HANG_RONG = "Lô hàng không được rỗng";
-    
+
     public ChiTietDonNhapHang() {
     }
 
     //ok
     public ChiTietDonNhapHang(int soLuong, double donGia, double thanhTien, DonNhapHang donNhapHang, LoHang loHang) {
-       // this.maChiTietDonNhapHang = maChiTietDonNhapHang;
+        // this.maChiTietDonNhapHang = maChiTietDonNhapHang;
         this.soLuong = soLuong;
         this.donGia = donGia;
         this.thanhTien = thanhTien;
@@ -49,13 +50,12 @@ public class ChiTietDonNhapHang {
 //        }
 //        this.maChiTietDonNhapHang = maChiTietDonNhapHang;
 //    }
-
     public int getSoLuong() {
         return soLuong;
     }
 
-    public void setSoLuong(int soLuong) throws Exception{
-        if(soLuong <= 0){
+    public void setSoLuong(int soLuong) throws Exception {
+        if (soLuong <= 0) {
             throw new Exception(SO_LUONG_SAI);
         }
         this.soLuong = soLuong;
@@ -65,8 +65,8 @@ public class ChiTietDonNhapHang {
         return donGia;
     }
 
-    public void setDonGia(double donGia) throws Exception{
-        if(donGia < 0){
+    public void setDonGia(double donGia) throws Exception {
+        if (donGia < 0) {
             throw new Exception(DON_GIA_SAI);
         }
         this.donGia = donGia;
@@ -76,8 +76,8 @@ public class ChiTietDonNhapHang {
         return thanhTien;
     }
 
-    public void setThanhTien(double thanhTien) throws Exception{
-        if(thanhTien < 0){
+    public void setThanhTien(double thanhTien) throws Exception {
+        if (thanhTien < 0) {
             throw new Exception(THANH_TIEN_SAI);
         }
         this.thanhTien = thanhTien;
@@ -153,8 +153,8 @@ public class ChiTietDonNhapHang {
         return donNhapHang;
     }
 
-    public void setDonNhapHang(DonNhapHang donNhapHang) throws Exception{
-        if(donNhapHang == null){
+    public void setDonNhapHang(DonNhapHang donNhapHang) throws Exception {
+        if (donNhapHang == null) {
             throw new Exception(DON_NHAP_HANG_RONG);
         }
         this.donNhapHang = donNhapHang;
@@ -164,8 +164,8 @@ public class ChiTietDonNhapHang {
         return loHang;
     }
 
-    public void setLoHang(LoHang loHang) throws Exception{
-        if(loHang == null){
+    public void setLoHang(LoHang loHang) throws Exception {
+        if (loHang == null) {
             throw new Exception(LO_HANG_RONG);
         }
         this.loHang = loHang;
@@ -173,19 +173,19 @@ public class ChiTietDonNhapHang {
 
     @Override
     public String toString() {
-        return "ChiTietDonNhapHang{" +
-                //"maChiTietDonNhapHang='" + maChiTietDonNhapHang + '\'' +
-                ", soLuong=" + soLuong +
-                ", donGia=" + donGia +
-                ", thanhTien=" + thanhTien +
-                ", tyLeChietKhau=" + tyLeChietKhau +
-                ", tienChietKhau=" + tienChietKhau +
-                ", thanhTienTinhThue=" + thanhTienTinhThue +
-                ", thueSuat=" + thueSuat +
-                ", tienThue=" + tienThue +
-                ", thanhTienSauThue=" + thanhTienSauThue +
-                ", donNhapHang=" + (donNhapHang != null ? donNhapHang.getMaDonNhapHang() : null) +
-                ", loHang=" + (loHang != null ? loHang.getMaLoHang() : null) +
-                '}';
+        return "ChiTietDonNhapHang{"
+                + //"maChiTietDonNhapHang='" + maChiTietDonNhapHang + '\'' +
+                ", soLuong=" + soLuong
+                + ", donGia=" + donGia
+                + ", thanhTien=" + thanhTien
+                + ", tyLeChietKhau=" + tyLeChietKhau
+                + ", tienChietKhau=" + tienChietKhau
+                + ", thanhTienTinhThue=" + thanhTienTinhThue
+                + ", thueSuat=" + thueSuat
+                + ", tienThue=" + tienThue
+                + ", thanhTienSauThue=" + thanhTienSauThue
+                + ", donNhapHang=" + (donNhapHang != null ? donNhapHang.getMaDonNhapHang() : null)
+                + ", loHang=" + (loHang != null ? loHang.getMaLoHang() : null)
+                + '}';
     }
 }
